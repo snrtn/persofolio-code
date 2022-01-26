44a3bc821c2b661ecde85cbc5c0081613e6f9e7f
snrtn <snrtn94@gmail.com>
2022-01-26 12:52:05 +0100

    add

---
+++ client/src/components/curriculum/exp.data.ts
@@ -0,0 +1,39 @@
+export const exp = [
+  {
+    title: 'Machine-outil équipe',
+    subtitle: 'Séoul (Corée du Sud / Dongdaemun-gu) – formation au sein de l’école',
+    data: 'Période : 2010-2012',
+    dect: 'Maintenance de la machine',
+    decta: 'Fabrication de pièces automobile/moto',
+    dectb: '',
+    img: 'https://images.pexels.com/photos/48799/drill-milling-milling-machine-drilling-48799.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  },
+  {
+    title: 'Armée de l’air',
+    subtitle:
+      '***** (Corée du Sud / **th Fighter Wing) – Service militaire obligatoire en Corée du Sud Mission : Sergent',
+    data: 'Période : 2013-2015',
+    dect: 'En charge de la maintenance technique des avions de chasse',
+    decta: 'En charge de l’inventaire, du nettoyage et de la distribution des armes',
+    dectb: '',
+    img: 'https://images.pexels.com/photos/733254/pexels-photo-733254.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  },
+  {
+    title: 'POSCO',
+    subtitle: 'Séoul (Corée du Sud / Gannam-gu) – Entreprise dans la production d’acier',
+    data: 'Période : 2015-2018',
+    dect: 'En charge de la protection du PDG',
+    decta: '',
+    dectb: '',
+    img: 'https://www.ild-security.com/wp-content/uploads/2018/01/20.jpg',
+  },
+  {
+    title: 'E.Leclerc',
+    subtitle: 'Montévrain – Grande surface',
+    data: 'Période : 2019-2020',
+    dect: 'Nettoyage des rayons',
+    decta: 'Mettre les aliments et produits en rayon',
+    dectb: 'Organisation du magasin',
+    img: 'https://images.pexels.com/photos/264636/pexels-photo-264636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  },
+];
---
+++ client/src/components/curriculum/info.style.ts
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+import { r768px, r1280px } from '../../style/responsive';
+import variable from '../../style/variable';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 85vh;
+  margin: 10px auto 0px auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 60px;
+  ${r768px({
+    width: '100%',
+    flexDirection: 'column',
+    padding: '0px 0px',
+  })}
+  ${r1280px({
+    padding: '0px 0px',
+  })}
+
+  .gallery {
+    width: 100%;
+    list-style: none;
+    padding: 0;
+    margin: 10px 0px;
+    display: flex;
+    height: 82vh;
+    ${r768px({
+      width: '100%',
+      flexDirection: 'column',
+    })}
+  }
+  .gallery li {
+    flex: 1;
+    transition: 0.5s;
+    position: relative;
+    overflow: hidden;
+  }
+  .gallery li:last-child {
+    border-right: none;
+  }
+  .gallery li:nth-child(1) {
+    background: url('https://pixabay.com/get/gf5c410abd86f48f60129895ab6ddf60142710ef381c8d035d424ec5971fcd139789ad6441de2b547ca26df3d6862c6e22369ee5a3239643dd3ae8611a2eadab2_1920.jpg')
+      no-repeat center center;
+  }
+  .gallery li:nth-child(2) {
+    background: url('https://images.pexels.com/photos/7988956/pexels-photo-7988956.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+      no-repeat center center;
+  }
+  .gallery li:nth-child(3) {
+    background: url('https://www.tourisme-bresil.com/wp-content/uploads/adobestock-215533187-1280x800.jpeg')
+      no-repeat center center;
+  }
+
+  .gallery:hover li {
+    flex: 1;
+  }
+  .gallery li:hover {
+    flex: 3;
+    filter: grayscale(1);
+  }
+
+  .content_info {
+    position: absolute;
+    background-color: #fff;
+    text-align: center;
+    height: 200px;
+    bottom: -200px;
+    transition: 0.5s;
+    padding: 30px;
+    box-sizing: border-box;
+    width: 100%;
+
+    h2 {
+      font-size: ${variable.d20px};
+      z-index: 5;
+    }
+
+    p {
+      z-index: 5;
+      padding-top: 20px;
+      font-weight: 300;
+      width: 400px;
+      font-size: ${variable.d18px};
+      text-align: left;
+      margin: auto;
+      ${r768px({
+        width: '100%',
+      })}
+    }
+  }
+
+  .gallery li:hover .content_info {
+    bottom: 0;
+    transition-delay: 0.5s;
+  }
+`;
